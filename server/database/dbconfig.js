9b0b09bae1ae9e7b45be13a0eb471cb002f0db6a
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-06-13 00:51:47 +0300

    Controllers added

---
+++ server/database/dbconfig.js
@@ -0,0 +1,21 @@
+const mysql2 = require("mysql2");
+require("dotenv").config();
+const dbConnection = mysql2.createPool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// dbConnection.execute("select 'start' ", (err, result) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+module.exports = dbConnection.promise();
