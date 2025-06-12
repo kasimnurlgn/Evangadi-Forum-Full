a3a8c6246dd9398be20768203c7f1c0d7fedc00e
nafkota <hiwotmelaku84@yahoo.com>
2025-06-12 18:08:17 -0400

    register added

---
+++ server/controller/userController.js
@@ -0,0 +1,46 @@
+// register function
+async function register(req, res) {
+  const { username, first_name, last_name, email, password } = req.body;
+  // Validate required fields
+  if (!username || !first_name || !last_name || !email || !password) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: "Bad Request",
+      message: "Please provide all required fields",
+    });
+  }
+  // Validate password length
+  if (password.length < 8) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: "Bad Request",
+      message: "Password must be at least 8 characters",
+    });
+  }
+  try {
+    // Check for existing user
+    const [existingUser] = await dbConnection.query(
+      "SELECT * FROM users WHERE username = ? OR email = ?",
+      [username, email]
+    );
+    if (existingUser.length > 0) {
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json({ error: "Conflict", message: "User already existed" });
+    }
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Insert user into our database
+    await dbConnection.query(
+      "INSERT INTO users (username, first_name, last_name, email, password) VALUES (?, ?, ?, ?, ?)",
+      [username, first_name, last_name, email, hashedPassword]
+    );
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error in register:", error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "Internal Server Error",
+      message: "An unexpected error occurred.",
+    });
+  }
+}
