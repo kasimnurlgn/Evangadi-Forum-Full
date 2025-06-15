85521e4750d81cd2e614180da3c96961821cae44
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-06-15 22:54:36 +0300

    often need

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -6,6 +6,9 @@ const dbConnection = mysql2.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false, // FreeDB often needs this
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
