8b702ad2a3e2ee63d132aff7ba91acc4320dedbb
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-06-16 09:33:54 +0300

    env credentials updated

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -5,21 +5,10 @@ const dbConnection = mysql2.createPool({
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  ssl: {
-    rejectUnauthorized: false, // FreeDB often needs this
-  },
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-
-// dbConnection.execute("select 'start' ", (err, result) => {
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     console.log(result);
-//   }
-// });
 
 module.exports = dbConnection.promise();
